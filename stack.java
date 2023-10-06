8a556fe8062e23f0d7576fa132ca0eeee9782a16
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-06 13:03:46 +0530

    docs : added stack file

---
+++ stack.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class stack {
+     public static void main(String[] args) {
+
+      Stack<String> stack = new Stack<String>();
+
+
+   
+    
+
+      stack.push("Tomato");
+      stack.push("Potato");
+      stack.push("Onion");
+      stack.push("Cucumber");
+
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+     
+
+
+
+
+
+}
